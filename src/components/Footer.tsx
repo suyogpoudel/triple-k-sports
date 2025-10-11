06719cdfb52f521479b5cd15a3af8d502995ab89
Suyog Poudel <suyogpoudel583@gmail.com>
2025-10-11 18:35:14 +0545

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,38 @@
-import { IconBrandFacebook, IconBrandInstagram } from "@tabler/icons-react";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+} from "@tabler/icons-react";
 
 const Footer = () => {
   return (
-    <div className="bg-gray-900 px-4 py-6 flex justify-between align-items text-gray-400">
-      <p>&copy; 2024 Triple K Sports. All Rights Reserved.</p>
+    <div className="bg-gray-900 px-4 py-6 flex lg:justify-between items-center text-gray-400 lg:flex-row flex-col gap-3">
+      <p>&copy; 2024 Triple K Sports.</p>
 
       <div className="flex gap-2">
-        <a target="_blank" href="https://www.facebook.com/TripleKSportss">
+        <a
+          className="hover:text-gray-300 transition-colors duration-200"
+          target="_blank"
+          href="https://www.facebook.com/TripleKSportss"
+        >
           <IconBrandFacebook />
         </a>
-        <a target="_blank" href="https://www.instagram.com/tripleksports/">
+        <a
+          className="hover:text-gray-300 transition-colors duration-200"
+          target="_blank"
+          href="https://www.instagram.com/tripleksports/"
+        >
           <IconBrandInstagram />
         </a>
       </div>
+      <a
+        href="https://www.linkedin.com/in/suyogpoudel01/"
+        className="flex gap-1 justify-center items-center hover:text-gray-300 transition-colors duration-200"
+        target="_blank"
+      >
+        Developer ID:
+        <IconBrandLinkedin />
+      </a>
     </div>
   );
 };
